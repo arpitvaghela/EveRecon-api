6d871a3a19390237654858fa39f11d31d4e22cc5
Yashraj Kakkad <yashrajkakkad@gmail.com>
2021-04-01 12:05:15 +0530

    fix: crud community using custom code, feat: partial update now works

--- api/everecon/schema_community.py
+++ api/everecon/schema_community.py
@@ -13,13 +13,14 @@ class CommunityType(DjangoObjectType):
 # Queries
 
 # Mutations
+# Using serializer
+# class CreateCommunity(SerializerMutation):
+#     class Meta:
+#         serializer_class = CommunitySerializer
+#         model_operations = ['create', 'update']
+#         lookup_field = 'id'
 
-class CreateCommunity(SerializerMutation):
-    class Meta:
-        serializer_class = CommunitySerializer
-        model_operations = ['create', 'update']
-        lookup_field = 'id'
-
+# Using ModelForm
 # class CommunityForm(ModelForm):
 #     class Meta:
 #         model = Community
@@ -32,11 +33,83 @@ class CreateCommunity(SerializerMutation):
 #     class Meta:
 #         form_class = CommunityForm
 
-class DeleteCommunity(graphene.Mutation):
-    ok = graphene.Boolean()
+    # name = models.CharField(max_length=255)
+    # description = models.TextField(blank=True) # Not setting null=True as a Community must have a description
+    # logo = models.ImageField(upload_to='images/community/logos/', null=True, blank=True)
+    # banner = models.ImageField(upload_to='images/community/banners/', null=True, blank=True)
+    # featured_video = models.URLField(blank=True, null=True) # TODO: Add a validation for YouTube URL
+    # # featured_video = models.FileField(upload_to='videos/community/featured/', blank=True, null=True, verbose_name="")
+    # events = models.ManyToManyField(Event, related_name="communities", blank=True)
+    # address = models.TextField(null=True, blank=True)
+    # city = models.CharField(max_length=255, null=True, blank=True)
+    # country = models.CharField(max_length=255, null=True, blank=True)
+    # email = models.EmailField(null=True, blank=True)
+    # members_count = models.IntegerField(blank=True, default=0) # TODO: Update this automatically
+    # website = models.URLField(null=True, blank=True)
+    # facebook = models.URLField(null=True, blank=True) # TODO: Validation for social media
+    # linkedin = models.URLField(null=True, blank=True)
+    # twitter = models.URLField(null=True, blank=True)
+    # instagram = models.URLField(null=True, blank=True)
+    # discord = models.URLField(null=True, blank=True)
+    # is_active = models.BooleanField(default=True)
+    # creation_time = models.TimeField(auto_now_add=True, blank=True)
+    # followers = models.ManyToManyField(User ,related_name="communities", blank=True)
 
+class CreateCommunity(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        description = graphene.String(required=True)
+        featured_video = graphene.String()
+        address = graphene.String()
+        city = graphene.String()
+        country = graphene.String()
+        email = graphene.String()
+        website = graphene.String()
+        facebook = graphene.String()
+        linkedin = graphene.String()
+        instagram = graphene.String()
+        discord = graphene.String()        
+    
+    community = graphene.Field(CommunityType)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        community = Community(**kwargs)
+        community.save()
+        return cls(community=community)
+
+class UpdateCommunity(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        description = graphene.String()
+        featured_video = graphene.String()
+        address = graphene.String()
+        city = graphene.String()
+        country = graphene.String()
+        email = graphene.String()
+        website = graphene.String()
+        facebook = graphene.String()
+        linkedin = graphene.String()
+        instagram = graphene.String()
+        discord = graphene.String()        
+
+    community = graphene.Field(CommunityType)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        id = kwargs.pop('id')
+        community = Community.objects.get(id=id)
+        for k, v in kwargs.items():
+            community.k = v
+        community.save()
+        return cls(community=community)
+
+class DeleteCommunity(graphene.Mutation):
     class Arguments:
         id = graphene.ID()
+
+    ok = graphene.Boolean()
 
     @classmethod
     def mutate(cls, root, info, **kwargs):
@@ -53,5 +126,7 @@ class Query(graphene.ObjectType):
 
 # Mutation Class
 class Mutation(graphene.ObjectType):
-    create_update_community = CreateCommunity.Field()
+    create_community = CreateCommunity.Field()
+    update_community = UpdateCommunity.Field()
+    # create_update_community = CreateCommunity.Field()
     delete_community = DeleteCommunity.Field()
