b8a24244222ed6354f8ef1a59d94439c046ada32
mrparth23 <mrparth2302@gmail.com>
2021-04-03 12:35:03 +0530

    added Validations for profile handle URLs

--- api/everecon/models.py
+++ api/everecon/models.py
@@ -2,7 +2,56 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils import dateformat, timezone
+from django.core.validators import RegexValidator
+
+
+# Validation for facebook handle URL
+def validation_facebook(facebook):
+    url_validator_message = 'Invalid handle URL!'
+    facebook_regex = "/(?:(?:http|https):\/\/)?(?:www\.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?"
+    url_validator = RegexValidator(regex=facebook_regex, message=url_validator_message)
+    return url_validator(facebook)
+
+
+# Validation for instagram handle URL
+def validation_instagram(instagram):
+    url_validator_message = 'Invalid handle URL!'
+    instagram_regex = "/(?:(?:http|https):\/\/)?(?:www\.)?(?:instagram\.com|instagr\.am)\/([A-Za-z0-9-_\.]+)?"
+    url_validator = RegexValidator(regex=instagram_regex, message=url_validator_message)
+    return url_validator(instagram)
+
+
+# Validation for twitter handle URL
+def validation_twitter(twitter):
+    url_validator_message = 'Invalid handle URL!'
+    twitter_regex = "/(?:(?:http|https):\/\/)?(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)?"
+    url_validator = RegexValidator(regex=twitter_regex, message=url_validator_message)
+    return url_validator(twitter)
+
+
+# Validation for linkedin handle URL
+def validation_linkedin(linkedin):
+    url_validator_message = 'Invalid handle URL!'
+    linkedin_regex = "/(?:(?:http|https):\/\/)?(?:www\.)?linkedin.com/((in/[^/]+/?)|(pub/[^/]+/((\w|\d)+/?){3}))$"
+    url_validator = RegexValidator(regex=linkedin_regex, message=url_validator_message)
+    return url_validator(linkedin)
+
+
+# Validation for discord handle URL
+def validation_discord(discord):
+    url_validator_message = 'Invalid handle URL!'
+    discord_regex = "/(?:(?:http|https):\/\/)?(?:www\.)?(discord.(gg|io|me|li)|discordapp.com/invite)/[^\s/]+?(?=\s) ^.{3,32}#[0-9]{4}$ "
+    url_validator = RegexValidator(regex=discord_regex, message=url_validator_message)
+    return url_validator(discord)
+
+
+# Validation for facebook handle URL
+def validation_youtube(youtube):
+    url_validator_message = 'Invalid handle URL!'
+    youtube_regex = "/(?:(?:http|https):\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+    url_validator = RegexValidator(regex=youtube_regex, message=url_validator_message)
+    return url_validator(youtube)
+
 
 # User Model, using Django's inbuilt model for now so ignore this
 # class User(models.Model):
@@ -64,9 +113,7 @@ class Event(models.Model):
     description = models.TextField(null=True, blank=True)
     type = models.CharField(max_length=255)  # Type should have options
     address = models.TextField(null=True, blank=True)
-    city = models.CharField(
-        max_length=255, null=True, blank=True
-    )  # Needs to be discussed
+    city = models.CharField(max_length=255, null=True, blank=True)  # Needs to be discussed
     country = models.CharField(max_length=255, null=True, blank=True)
     live_URL = models.URLField(null=True, blank=True)  # Either of the two must be there
     # category = models.CharField(max_length=255)
@@ -109,28 +156,26 @@ class Speaker(models.Model):
 
 class Community(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True) # Not setting null=True as a Community must have a description
+    description = models.TextField(blank=True)  # Not setting null=True as a Community must have a description
     logo = models.ImageField(upload_to='images/community/logos/', null=True, blank=True)
     banner = models.ImageField(upload_to='images/community/banners/', null=True, blank=True)
-    featured_video = models.URLField(blank=True, null=True) # TODO: Add a validation for YouTube URL
+    featured_video = models.URLField(blank=True, null=True, validators=[validation_youtube])
     # featured_video = models.FileField(upload_to='videos/community/featured/', blank=True, null=True, verbose_name="")
     events = models.ManyToManyField(Event, related_name="communities", blank=True)
     address = models.TextField(null=True, blank=True)
     city = models.CharField(max_length=255, null=True, blank=True)
     country = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    members_count = models.IntegerField(blank=True, default=0) # TODO: Update this automatically
+    members_count = models.IntegerField(blank=True, default=0)  # TODO: Update this automatically
     website = models.URLField(null=True, blank=True)
-    facebook = models.URLField(
-        null=True, blank=True
-    )  # TODO: Validation for social media
-    linkedin = models.URLField(null=True, blank=True)
-    twitter = models.URLField(null=True, blank=True)
-    instagram = models.URLField(null=True, blank=True)
-    discord = models.URLField(null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True, validators=[validation_facebook])
+    linkedin = models.URLField(null=True, blank=True, validators=[validation_linkedin])
+    twitter = models.URLField(null=True, blank=True, validators=[validation_twitter])
+    instagram = models.URLField(null=True, blank=True, validators=[validation_instagram])
+    discord = models.URLField(null=True, blank=True, validators=[validation_discord])
     is_active = models.BooleanField(default=True)
     creation_time = models.TimeField(auto_now_add=True, blank=True)
-    followers = models.ManyToManyField(User ,related_name="communities", blank=True)
+    followers = models.ManyToManyField(User, related_name="communities", blank=True)
     leader = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -140,16 +185,14 @@ class Community(models.Model):
 class Sponsor(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    logo = models.ImageField(upload_to="images/sponsor/logos/", null=True, blank=True)
-    banner = models.ImageField(
-        upload_to="images/community/banners/", null=True, blank=True
-    )
-    communities = models.ManyToManyField(
-        Community, related_name="sponsors", blank=True
-    )  # Sponsors sponsor communities, not events
+    logo = models.ImageField(upload_to='images/sponsor/logos/', null=True, blank=True)
+    banner = models.ImageField(upload_to='images/community/banners/', null=True, blank=True)
+    communities = models.ManyToManyField(Community, related_name='sponsors',
+                                         blank=True)  # Sponsors sponsor communities, not events
 
     def __str__(self):
         return self.name
+
 
 # class CommunityLeader(models.Model):
 #     creation_time = models.TimeField(auto_now_add=True, blank=True)
